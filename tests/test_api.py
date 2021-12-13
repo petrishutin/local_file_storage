29014475790fbb468947d5d6d9d1da47871694a9
petr <xmf@inbox.ru>
2021-12-13 22:11:15 +0300

    some tests refactoring (removed chain of tests)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 
 from requests.auth import HTTPBasicAuth
@@ -20,33 +18,33 @@ def basic_auth():
     return HTTPBasicAuth(config.STORAGE_USER, config.PASSWORD)
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture()
 def txt_file():
     with open(f"{config.BASE_DIR}/../tests/test.txt", "rb") as txt_file:
         yield txt_file
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture()
 def odt_file():
     with open(f"{config.BASE_DIR}/../tests/test.odt", "rb") as odt_file:
         yield odt_file
 
 
-@pytest.fixture(scope='module')
-def new_file_hash():
-    return {}
+@pytest.fixture()
+def new_file_hash(client, txt_file, basic_auth):
+    response = client.post('/upload', auth=basic_auth, files={'uploaded_file': txt_file})
+    return response.text.replace('"', '')
 
 
-def test_upload_201(client, basic_auth, txt_file, new_file_hash):
+def test_upload_201(client, basic_auth, txt_file, ):
     response = client.post('/upload', auth=basic_auth, files={'uploaded_file': txt_file})
     assert response.status_code == 201, response.text
     file_hash = response.text
     assert file_hash
-    new_file_hash['file_hash'] = file_hash.replace('"', '')
 
 
 def test_download_200(client, basic_auth, txt_file, new_file_hash):
-    response = client.get(f"/download/{new_file_hash['file_hash']}", auth=basic_auth)
+    response = client.get(f"/download/{new_file_hash}", auth=basic_auth)
     assert response.status_code == 200, response.text
     txt_file.seek(0)
     assert txt_file.read() == response.content
@@ -54,29 +52,24 @@ def test_download_200(client, basic_auth, txt_file, new_file_hash):
 
 def test_update_200(client, basic_auth, odt_file, new_file_hash):
     response = client.put(
-        f"/update/",
-        auth=basic_auth,
-        files={'uploaded_file': odt_file},
-        data={'file_hash': new_file_hash['file_hash']}
+        f"/update/", auth=basic_auth, files={'uploaded_file': odt_file}, data={'file_hash': new_file_hash}
     )
     assert response.status_code == 200, response.text
-    assert response.text.replace('"', '') == f"file {new_file_hash['file_hash']} successfully updated"
-
-
-def test_file_is_updated(client, basic_auth, odt_file, new_file_hash):
-    response = client.get(f"/download/{new_file_hash['file_hash']}", auth=basic_auth)
+    assert response.text.replace('"', '') == f"file {new_file_hash} successfully updated"
+    response = client.get(f"/download/{new_file_hash}", auth=basic_auth)
     assert response.status_code == 200, response.text
     odt_file.seek(0)
     assert odt_file.read() == response.content
 
 
 def test_delete_200(client, basic_auth, new_file_hash):
-    response = client.delete(f"/delete/{new_file_hash['file_hash']}", auth=basic_auth)
+    response = client.delete(f"/delete/{new_file_hash}", auth=basic_auth)
     assert response.status_code == 200, response.text
-    assert response.text.replace('"', '') == f"file {new_file_hash['file_hash']} successfully deleted"
+    assert response.text.replace('"', '') == f"file {new_file_hash} successfully deleted"
 
 
 def test_deleted_file_not_found_404(client, basic_auth, new_file_hash):
-    response = client.get(f"/download/{new_file_hash['file_hash']}", auth=basic_auth)
+    client.delete(f"/delete/{new_file_hash}", auth=basic_auth)
+    response = client.get(f"/download/{new_file_hash}", auth=basic_auth)
     assert response.status_code == 404, response.text
-    assert response.json()['detail'] == f"file {new_file_hash['file_hash']} not found", response.text
+    assert response.json()['detail'] == f"file {new_file_hash} not found", response.text
